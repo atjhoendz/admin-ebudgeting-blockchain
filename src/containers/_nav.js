ad2b0853912e252d61d94aa3085d5f3b4c3687bc
atjhoendz <achunarmando@gmail.com>
2021-01-20 22:16:45 +0700

    feat: add dashboard page

--- src/containers/_nav.js
+++ src/containers/_nav.js
@@ -14,13 +14,117 @@ export default [
       },
       {
         _name: 'CSidebarNavDropdown',
-        name: 'Data Pemohon',
-        route: '/',
-        icon: 'cil-drop',
+        name: 'Data Provinsi',
+        to: '/data-provinsi',
+        icon: 'cil-building',
         items: [
           {
+            name: 'Lihat Data',
+            to: '/data-provinsi'
+          },
+          {
             name: 'Tambah Data',
-            to: '/'
+            to: '/data-provinsi/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data Kategori',
+        to: '/data-kategori',
+        icon: 'cil-library',
+        items: [
+          {
+            name: 'Lihat Data',
+            to: '/data-kategori'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-kategori/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data Lembaga',
+        to: '/data-lembaga',
+        icon: 'cil-institution',
+        items: [
+          {
+            name: 'Lihat Data',
+            to: '/data-lembaga'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-lembaga/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data Anggaran',
+        to: '/data-anggaran',
+        icon: 'cil-dollar',
+        items: [
+          {
+            name: 'Lihat Data',
+            to: '/data-anggaran'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-anggaran/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data PMK',
+        to: '/data-pmk',
+        icon: 'cil-list',
+        items: [
+          {
+            name: 'Lihat Data',
+            to: '/data-pmk'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-pmk/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data Penomoran',
+        to: '/data-penomoran',
+        icon: 'cil-list-numbered',
+        items: [
+          {
+            name: 'Lihat Data',
+            to: '/data-penomoran'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-penomoran/tambah-data'
+          }
+        ]
+      },
+      {
+        _name: 'CSidebarNavTitle',
+        _children: ['User']
+      },
+      {
+        _name: 'CSidebarNavDropdown',
+        name: 'Data Pengguna',
+        to: '/data-pengguna',
+        icon: 'cil-user',
+        items: [
+          {
+            name: 'Daftar Pengguna',
+            to: '/data-pengguna'
+          },
+          {
+            name: 'Tambah Data',
+            to: '/data-pengguna/tambah-data'
           }
         ]
       },
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,124 @@
 <template>
-  <div>
-    Hello World
-  </div>
+  <CRow>
+    <CCol sm="12" lg="6">
+      <CWidgetSimple header="Message" text="Selamat Datang di Halaman Admin">
+      </CWidgetSimple>
+    </CCol>
+    <CCol sm="12" lg="3" v-for="data in dataDashboard" :key="data.title">
+      <CWidgetSimple :header="data.title" :text="data.text"> </CWidgetSimple>
+    </CCol>
+    <CCol col="12">
+      <CCard>
+        <CCardHeader class="h5">
+          <strong>Data Pengguna</strong>
+        </CCardHeader>
+        <CCardBody>
+          <CDataTable
+            :items="items"
+            :fields="fields"
+            column-filter
+            table-filter
+            items-per-page-select
+            :items-per-page="5"
+            hover
+            sorter
+            pagination
+          >
+          </CDataTable>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
 </template>
 
 <script>
+const items = [
+  { username: "Samppa Nori", registered: "2012/01/01", role: "Member" },
+  { username: "Estavan Lykos", registered: "2012/02/01", role: "Staff" },
+  { username: "Chetan Mohamed", registered: "2012/02/01", role: "Admin" },
+  { username: "Derick Maximinus", registered: "2012/03/01", role: "Member" },
+  { username: "Friderik Dávid", registered: "2012/01/21", role: "Staff" },
+  { username: "Yiorgos Avraamu", registered: "2012/01/01", role: "Member" },
+  {
+    username: "Avram Tarasios",
+    registered: "2012/02/01",
+    role: "Staff",
+    _classes: "table-success",
+  },
+  { username: "Quintin Ed", registered: "2012/02/01", role: "Admin" },
+  { username: "Enéas Kwadwo", registered: "2012/03/01", role: "Member" },
+  { username: "Agapetus Tadeáš", registered: "2012/01/21", role: "Staff" },
+  {
+    username: "Carwyn Fachtna",
+    registered: "2012/01/01",
+    role: "Member",
+    _classes: "table-info",
+  },
+  { username: "Nehemiah Tatius", registered: "2012/02/01", role: "Staff" },
+  { username: "Ebbe Gemariah", registered: "2012/02/01", role: "Admin" },
+  { username: "Eustorgios Amulius", registered: "2012/03/01", role: "Member" },
+  { username: "Leopold Gáspár", registered: "2012/01/21", role: "Staff" },
+  { username: "Pompeius René", registered: "2012/01/01", role: "Member" },
+  { username: "Paĉjo Jadon", registered: "2012/02/01", role: "Staff" },
+  { username: "Micheal Mercurius", registered: "2012/02/01", role: "Admin" },
+  { username: "Ganesha Dubhghall", registered: "2012/03/01", role: "Member" },
+  { username: "Hiroto Šimun", registered: "2012/01/21", role: "Staff" },
+  { username: "Vishnu Serghei", registered: "2012/01/01", role: "Member" },
+  { username: "Zbyněk Phoibos", registered: "2012/02/01", role: "Staff" },
+  {
+    username: "Einar Randall",
+    registered: "2012/02/01",
+    role: "Admin",
+    _classes: "table-danger",
+  },
+  { username: "Félix Troels", registered: "2012/03/21", role: "Staff" },
+  { username: "Aulus Agmundr", registered: "2012/01/01", role: "Member" },
+];
+
+const fields = [
+  { key: "username", _style: "min-width:200px" },
+  "registered",
+  { key: "role", _style: "min-width:100px" },
+];
+
+const dataDashboard = [
+  {
+    title: "Data Provinsi",
+    text: "34",
+  },
+  {
+    title: "Data Kategori",
+    text: "10",
+  },
+  {
+    title: "Data Lembaga",
+    text: "16",
+  },
+  {
+    title: "Data Anggaran",
+    text: "20",
+  },
+  {
+    title: "Data PMK",
+    text: "5",
+  },
+  {
+    title: "Data Penomoran",
+    text: "5",
+  },
+];
+
 export default {
   name: "Dashboard",
   components: {},
   data() {
-    return {};
+    return {
+      dataDashboard,
+      items: items.map((item) => {
+        return { ...item };
+      }),
+      fields,
+    };
   },
   methods: {},
 };
