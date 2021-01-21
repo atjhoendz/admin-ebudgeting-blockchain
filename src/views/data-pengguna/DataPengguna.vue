3c9942bc2c1086ca1026529329a0c72c59de5c03
atjhoendz <achunarmando@gmail.com>
2021-01-21 18:21:59 +0700

    ref: move to listdata component

--- src/views/data-pengguna/DataPengguna.vue
+++ src/views/data-pengguna/DataPengguna.vue
@@ -1,96 +1,13 @@
 <template>
-  <CRow>
-    <CCol col="12">
-      <CCard>
-        <CCardHeader class="d-flex">
-          <span class="h4 mr-auto my-auto">Data Pengguna</span>
-          <CButton
-            color="primary"
-            class="d-md-down-none"
-            to="/data-pengguna/tambah"
-            >Tambah Data</CButton
-          >
-          <CButton color="primary" class="d-lg-none" to="/data-pengguna/tambah">
-            <CIcon name="cil-plus" />
-          </CButton>
-        </CCardHeader>
-        <CCardBody>
-          <CDataTable
-            :items="items"
-            :fields="fields"
-            table-filter
-            items-per-page-select
-            :items-per-page="5"
-            hover
-            sorter
-            pagination
-          >
-            <template #proses="{index}">
-              <td>
-                <CButtonGroup>
-                  <CButton color="success" :to="editByID(index)">Edit</CButton>
-                  <CButton color="danger" :to="deleteByID(index)"
-                    >Hapus</CButton
-                  >
-                </CButtonGroup>
-              </td>
-            </template>
-            <template #no="{index}">
-              <td>
-                {{ index + 1 }}
-              </td>
-            </template>
-          </CDataTable>
-        </CCardBody>
-      </CCard>
-    </CCol>
-  </CRow>
+  <list-data
+    :items="items"
+    :fields="fields"
+    title="Data Pengguna"
+    routeEndpoint="data-pengguna"
+  ></list-data>
 </template>
 
 <script>
-const items = [
-  { username: "Samppa Nori", registered: "2012/01/01", role: "Member" },
-  { username: "Estavan Lykos", registered: "2012/02/01", role: "Staff" },
-  { username: "Chetan Mohamed", registered: "2012/02/01", role: "Admin" },
-  { username: "Derick Maximinus", registered: "2012/03/01", role: "Member" },
-  { username: "Friderik Dávid", registered: "2012/01/21", role: "Staff" },
-  { username: "Yiorgos Avraamu", registered: "2012/01/01", role: "Member" },
-  {
-    username: "Avram Tarasios",
-    registered: "2012/02/01",
-    role: "Staff",
-    _classes: "table-success",
-  },
-  { username: "Quintin Ed", registered: "2012/02/01", role: "Admin" },
-  { username: "Enéas Kwadwo", registered: "2012/03/01", role: "Member" },
-  { username: "Agapetus Tadeáš", registered: "2012/01/21", role: "Staff" },
-  {
-    username: "Carwyn Fachtna",
-    registered: "2012/01/01",
-    role: "Member",
-    _classes: "table-info",
-  },
-  { username: "Nehemiah Tatius", registered: "2012/02/01", role: "Staff" },
-  { username: "Ebbe Gemariah", registered: "2012/02/01", role: "Admin" },
-  { username: "Eustorgios Amulius", registered: "2012/03/01", role: "Member" },
-  { username: "Leopold Gáspár", registered: "2012/01/21", role: "Staff" },
-  { username: "Pompeius René", registered: "2012/01/01", role: "Member" },
-  { username: "Paĉjo Jadon", registered: "2012/02/01", role: "Staff" },
-  { username: "Micheal Mercurius", registered: "2012/02/01", role: "Admin" },
-  { username: "Ganesha Dubhghall", registered: "2012/03/01", role: "Member" },
-  { username: "Hiroto Šimun", registered: "2012/01/21", role: "Staff" },
-  { username: "Vishnu Serghei", registered: "2012/01/01", role: "Member" },
-  { username: "Zbyněk Phoibos", registered: "2012/02/01", role: "Staff" },
-  {
-    username: "Einar Randall",
-    registered: "2012/02/01",
-    role: "Admin",
-    _classes: "table-danger",
-  },
-  { username: "Félix Troels", registered: "2012/03/21", role: "Staff" },
-  { username: "Aulus Agmundr", registered: "2012/01/01", role: "Member" },
-];
-
 const fields = [
   { key: "no", _style: "width:10px" },
   { key: "username", _style: "min-width:200px" },
@@ -99,23 +16,21 @@ const fields = [
   { key: "proses", _style: "width:50px" },
 ];
 
+import ListData from "../../components/ListData";
+import { itemsPengguna } from "../../sample-data/data";
+
 export default {
   name: "DataPengguna",
+  components: {
+    ListData,
+  },
   data() {
     return {
-      items: items.map((item) => {
+      items: itemsPengguna.map((item) => {
         return { ...item };
       }),
       fields,
     };
-  },
-  methods: {
-    editByID(id) {
-      return `/data-pengguna/edit?id=${id}`;
-    },
-    deleteByID(id) {
-      return `/data-pengguna/hapus?id=${id}`;
-    },
   },
 };
 </script>
--- src/views/data-provinsi/DataProvinsi.vue
+++ src/views/data-provinsi/DataProvinsi.vue
@@ -1,89 +1,16 @@
 <template>
-  <CRow>
-    <CCol col="5">
-      <CCard>
-        <CCardHeader class="d-flex">
-          <span class="h4 mr-auto my-auto">Data Provinsi</span>
-          <CButton
-            color="primary"
-            class="d-md-down-none"
-            to="/data-provinsi/tambah"
-            >Tambah Data</CButton
-          >
-          <CButton color="primary" class="d-lg-none" to="/data-provinsi/tambah">
-            <CIcon name="cil-plus" />
-          </CButton>
-        </CCardHeader>
-        <CCardBody>
-          <CDataTable
-            :items="items"
-            :fields="fields"
-            table-filter
-            items-per-page-select
-            :items-per-page="5"
-            hover
-            sorter
-            pagination
-          >
-            <template #proses="{index}">
-              <td>
-                <CButtonGroup>
-                  <CButton color="success" :to="editByID(index)">Edit</CButton>
-                  <CButton color="danger" :to="deleteByID(index)"
-                    >Hapus</CButton
-                  >
-                </CButtonGroup>
-              </td>
-            </template>
-            <template #no="{index}">
-              <td>
-                {{ index + 1 }}
-              </td>
-            </template>
-          </CDataTable>
-        </CCardBody>
-      </CCard>
-    </CCol>
-  </CRow>
+  <list-data
+    :items="items"
+    :fields="fields"
+    :width="5"
+    title="Data Provinsi"
+    routeEndpoint="data-provinsi"
+  ></list-data>
 </template>
 
 <script>
-const items = [
-  { "nama-provinsi": "Samppa Nori" },
-  { "nama-provinsi": "Estavan Lykos" },
-  { "nama-provinsi": "Chetan Mohamed" },
-  { "nama-provinsi": "Derick Maximinus" },
-  { "nama-provinsi": "Friderik Dávid" },
-  { "nama-provinsi": "Yiorgos Avraamu" },
-  {
-    "nama-provinsi": "Avram Tarasios",
-    _classes: "table-success",
-  },
-  { "nama-provinsi": "Quintin Ed" },
-  { "nama-provinsi": "Enéas Kwadwo" },
-  { "nama-provinsi": "Agapetus Tadeáš" },
-  {
-    "nama-provinsi": "Carwyn Fachtna",
-    _classes: "table-info",
-  },
-  { "nama-provinsi": "Nehemiah Tatius" },
-  { "nama-provinsi": "Ebbe Gemariah" },
-  { "nama-provinsi": "Eustorgios Amulius" },
-  { "nama-provinsi": "Leopold Gáspár" },
-  { "nama-provinsi": "Pompeius René" },
-  { "nama-provinsi": "Paĉjo Jadon" },
-  { "nama-provinsi": "Micheal Mercurius" },
-  { "nama-provinsi": "Ganesha Dubhghall" },
-  { "nama-provinsi": "Hiroto Šimun" },
-  { "nama-provinsi": "Vishnu Serghei" },
-  { "nama-provinsi": "Zbyněk Phoibos" },
-  {
-    "nama-provinsi": "Einar Randall",
-    _classes: "table-danger",
-  },
-  { "nama-provinsi": "Félix Troels" },
-  { "nama-provinsi": "Aulus Agmundr" },
-];
+import ListData from "../../components/ListData.vue";
+import { itemsProvinsi } from "../../sample-data/data";
 
 const fields = [
   { key: "no", _style: "width:10px" },
@@ -92,22 +19,15 @@ const fields = [
 ];
 
 export default {
+  components: { ListData },
   name: "DataProvinsi",
   data() {
     return {
-      items: items.map((item) => {
+      items: itemsProvinsi.map((item) => {
         return { ...item };
       }),
       fields,
     };
-  },
-  methods: {
-    editByID(id) {
-      return `/data-provinsi/edit?id=${id}`;
-    },
-    deleteByID(id) {
-      return `/data-provinsi/hapus?id=${id}`;
-    },
   },
 };
 </script>
