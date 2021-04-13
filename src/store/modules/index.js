bd051a5b8bff99086e1c015c4e054972100ecf97
atjhoendz <achunarmando@gmail.com>
2021-04-13 10:24:25 +0700

    chore(state): remove unused state module

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,10 +1,8 @@
-import auth from './auth';
 import dashboard from './dashboard';
 import user from './user';
 import toast from './toast';
 
 export default {
-  auth,
   dashboard,
   user,
   toast,
