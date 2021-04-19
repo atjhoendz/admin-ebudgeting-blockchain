7cb3536d6156cb631342c9c4f61748b83d2fc0cc
atjhoendz <achunarmando@gmail.com>
2021-04-19 10:15:00 +0700

    chore(header): handle catch error on logout

--- src/containers/TheHeaderDropdownAccnt.vue
+++ src/containers/TheHeaderDropdownAccnt.vue
@@ -1,24 +1,31 @@
 <template>
-  <CDropdown inNav class="c-header-nav-items" placement="bottom">
-    <template #toggler-content>
-      <span class="h4" style="color: #3c4b64">{{
-        $store.state.user.currentUser.username
-      }}</span>
-    </template>
-    <CDropdownItem @click="makeLogout()">
-      <CIcon name="cil-lock-locked" /> Logout
-    </CDropdownItem>
-  </CDropdown>
+  <div>
+    <CDropdown inNav class="c-header-nav-items" placement="bottom">
+      <template #toggler-content>
+        <span class="h4" style="color: #3c4b64">{{
+          $store.state.user.currentUser.username
+        }}</span>
+      </template>
+      <CDropdownItem @click="makeLogout()">
+        <CIcon name="cil-lock-locked" /> Logout
+      </CDropdownItem>
+    </CDropdown>
+    <toast-msg :listToasts="listToasts" />
+  </div>
 </template>
 
 <script>
 import { AuthService } from '../services/auth.service';
+import ToastMsg from '../components/ToastMsg';
 
 export default {
   name: 'TheHeaderDropdownAccnt',
+  components: {
+    ToastMsg,
+  },
   data() {
     return {
-      itemsCount: 42,
+      listToasts: [],
     };
   },
   methods: {
@@ -26,7 +33,10 @@ export default {
       try {
         await AuthService.makeLogout();
       } catch (err) {
-        console.log(err);
+        this.listToasts.push({
+          message: 'Terjadi masalah. Logout tidak berhasil.',
+          color: 'danger',
+        });
       }
     },
   },
